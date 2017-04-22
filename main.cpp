f7c29c325cf2d1a5a9449781266663e914031318
Aleksandar Babic <aleksandar.babic@gmail.com>
2017-04-22 11:58:32 +0200

    Save images to local "data" dir; just to test

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+//    QCoreApplication a(argc, argv);
 
     xrf::CineLoop loop = xrf::CineLoop::Create("C:/dev/xrfdump/data/pat4_biplaneA.dcm");
-    if(loop.IsValid())
+    if(loop.IsValid()) {
         for(auto tag : loop.GetDcmValues().keys())
             qDebug() << xrf::DcmTagIdToString[tag] << loop.GetDcmValues()[tag];
+
+        int frmNo = 0;
+        for(auto frame : loop.GetFrames())
+            frame->save("C:/dev/xrfdump/data/img_frm_" + QString::number(frmNo++) + ".png", "PNG");
+    }
 
     //return a.exec();
 }
